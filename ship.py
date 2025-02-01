3c169ccb7875cf2c71f47c87ee8da4726c3bc869
Shang Jin <lzhsj0219@outlook.com>
2025-02-01 13:11:32 +0800

    三次提交

--- ship.py
+++ ship.py
@@ -8,12 +8,28 @@ class Ship:
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
 
-        #加载飞船图像并获取其外接矩形
+        # 加载飞船图像并获取其外接矩形
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
 
-        #每艘新飞船都放在屏幕底部的中央
+        # 每艘新飞船都放在屏幕底部的中央
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # 在飞船的属性 x 中存储一个浮点数
+        self.x = float(self.rect.x)
+
+
+        # 移动标志（飞船一开始不移动）
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """根据移动标志调整飞船的位置"""
+        # 更新飞船的属性 x 的值，而不是其外接矩形的属性 x 的值
+        if self.moving_right:
+            self.x += self.settings.ship_speed
+        if self.moving_left:
+            self.x -= self.settings.ship_speed
 
     def blitme(self):
         """在指定位置绘画飞船"""
